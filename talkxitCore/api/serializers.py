bf3d45007dac3463aa3984ff751778454702e635
Linkzandro <lizandrolgui123@gmail.com>
2025-06-10 15:55:36 -0300

    feature: user modelviewset

--- talkxitCore/api/serializers.py
+++ talkxitCore/api/serializers.py
@@ -7,8 +7,14 @@ class UserProfileCreateSerializer(serializers.ModelSerializer):
     password2=serializers.CharField(write_only=True,required=True)
     class Meta:
         model=UserProfile
-        fields = ['name', 'email', 'password','password2','birthday']
+        fields = ['id','username','name','surname','email', 'password','password2','birthday']
+        read_only_fields=['id']
     def validate(self, attrs):
         if attrs['password']!=attrs['password2']:
             raise serializers.ValidationError('senhas não coincidem')
         return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password2')
+
+        return UserProfile.objects.create_user(**validated_data)
